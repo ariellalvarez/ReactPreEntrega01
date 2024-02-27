dada47f23d740c3a728ddee423efdc4f4ebf4962
ariellalvarez <alvarez.ariel@outlook.com>
2024-02-26 21:41:32 -0300

    PreEntrega01 v2

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,9 @@
-import cart from './assets/cart.jpg'
+import cart from './assets/cart2.png'
 
 const CartWidget = () => {
     return (
         <div>
-            <img src="{cart}" alt="cart-widget"/>
+            <img src={cart} alt="cart-widget"/>
             0
         </div>
     )
