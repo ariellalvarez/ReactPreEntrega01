8c7320d1f8b122265713e0240631b687877dc544
ariellalvarez <alvarez.ariel@outlook.com>
2024-03-14 00:53:33 -0300

    02 v2

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,14 @@
+const ItemList =({ products}) =>{
+
+    return(
+        <section>
+            {
+                products.map(product =>{
+                    return <p key={product.id}>{product.name}</p>
+                })
+            }
+        </section>
+    )
+}
+
+export default ItemList
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,24 @@
 import classes from '../css/style.module.css'
+import { useEffect, useState } from 'react'
+import { getProducts } from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
+
 
 const ItemListContainer = ({ greeting}) => {
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+        getProducts()
+            .then(result =>{
+                setProducts(result)
+            })
+})
+
+
     return (
         <main>
             <h1 className={classes.h1}>{greeting}</h1>
+            <ItemList products={products}/>
         </main>
     )
 }
